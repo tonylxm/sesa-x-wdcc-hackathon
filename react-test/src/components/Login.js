909a1397d3dd927145bcfe8038434deeceef158b
John <codecreator127@gmail.com>
2023-08-01 20:47:06 +1200

    styled the login and sign up

--- react-test/src/components/Login.js
+++ react-test/src/components/Login.js
@@ -22,42 +22,46 @@ function Login() {
   };
 
   return (
-    <div className="container mx-auto py-8">
-      <h2 className="text-2xl font-bold mb-4">Login</h2>
-      <form onSubmit={handleSubmit} className="max-w-sm">
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700">Email</label>
-          <input
-            type="email"
-            id="email"
-            className="border border-gray-400 p-2 w-full"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="password" className="block text-gray-700">Password</label>
-          <input
-            type="password"
-            id="password"
-            className="border border-gray-400 p-2 w-full"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-        >
-          Log In
-        </button>
+    <div className="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0">
+      <main className="max-w-sm mx-auto bg-white p-8 rounded-lg shadow-2xl">
+        <h2 className="text-2xl font-bold mb-4">Login</h2>
+        <form onSubmit={handleSubmit} className="flex flex-col">
+          <div className="mb-6 pt-3 rounded bg-gray-200">
+            <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2 ml-3">Email</label>
+            <input
+              type="email"
+              id="email"
+              className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-6 pt-3 rounded bg-gray-200">
+            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2 ml-3">Password</label>
+            <input
+              type="password"
+              id="password"
+              className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200"
+          >
+            Log In
+          </button>
 
-        
-        <p> Don't have an account yet? </p>
-        <Link to="/signup"> Sign Up Here </Link>
-      </form>
+          <p className="mt-4 mx-auto">Don't have an account yet?</p>
+          <Link to="/signup" className="mx-auto text-purple-600 hover:text-purple-700 font-bold">
+            Sign Up Here
+          </Link>
+        </form>
+      </main>
     </div>
   );
-}
+};
+
 
 export default Login;
--- react-test/src/components/SignUp.js
+++ react-test/src/components/SignUp.js
@@ -35,52 +35,56 @@ function SignUp() {
         }
     };
 
-  return (
-    <div className="container mx-auto py-8">
-      <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
-      <form onSubmit={handleSubmit} className="max-w-sm">
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700">Email</label>
-          <input
-            type="email"
-            id="email"
-            className="border border-gray-400 p-2 w-full"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-            <label htmlFor="name" className="block text-gray-700">Name</label>
-            <input
+    return (
+      <div className="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0">
+        <main className="max-w-sm mx-auto bg-white p-8 rounded-lg shadow-2xl">
+          <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
+          <form onSubmit={handleSubmit} className="flex flex-col">
+            <div className="mb-6 pt-3 rounded bg-gray-200">
+              <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2 ml-3">Email</label>
+              <input
+                type="email"
+                id="email"
+                className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mb-6 pt-3 rounded bg-gray-200">
+              <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2 ml-3">Name</label>
+              <input
                 type="text"
                 id="name"
-                className="border border-gray-400 p-2 w-full"
+                className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
-            />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="password" className="block text-gray-700">Password</label>
-          <input
-            type="password"
-            id="password"
-            className="border border-gray-400 p-2 w-full"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-        >
-          Sign Up
-        </button>
-
-        <p>Already have an account? </p>
-        <Link to="/login"> Login Here </Link>
-      </form>
-    </div>
-  );
-}
+              />
+            </div>
+            <div className="mb-6 pt-3 rounded bg-gray-200">
+              <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2 ml-3">Password</label>
+              <input
+                type="password"
+                id="password"
+                className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white font-bold py-2 rounded hover:bg-blue-600"
+            >
+              Sign Up
+            </button>
+  
+            <p className="mt-4 mx-auto">Already have an account?</p>
+            <Link to="/login" className="text-blue-500 mx-auto hover:text-blue-600 font-bold">
+              Login Here
+            </Link>
+          </form>
+        </main>
+      </div>
+    );
+  };
 
 export default SignUp;
