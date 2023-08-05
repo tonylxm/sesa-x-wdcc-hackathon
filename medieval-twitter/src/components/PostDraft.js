3f6701f6fb9bea28abb960436675207d2be8d460
tonylxm <126369686+tonylxm@users.noreply.github.com>
2023-08-05 16:17:37 +1200

    added ExternalFiles bar to upload images and gifs

--- medieval-twitter/src/components/PostDraft.js
+++ medieval-twitter/src/components/PostDraft.js
@@ -1,24 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import { Picture, NameUsername, Post } from './Post';
+import { PiThumbsUp, PiThumbsDown, PiThumbsUpFill, PiThumbsDownFill } from 'react-icons/pi';
+import { BsImage, BsImageFill} from 'react-icons/bs';
+import { BiCommentDetail, BiSolidCommentDots } from 'react-icons/bi';
 import { db } from '../firebase'; // Import your Firebase configuration file
 
 
 const TextArea = () => {
-    return (
-        <div className=''>
-            <textarea placeholder='Start your post here...'></textarea>
-        </div>
+    var wordLen = 255; // Maximum word length
+    // function checkWordLen(obj) {
+    //     var len = obj.value.split(/[\s]+/);
+    //     if (len.length > wordLen) {
+    //         alert("You cannot put more than " + wordLen + " words in this text area.");
+    //         obj.oldValue = obj.value != obj.oldValue ? obj.value : obj.oldValue;
+    //         obj.value = obj.oldValue ? obj.oldValue : "";
+    //         return false;
+    //     }
+    //     return true;
+    // }
+    // onKeyDown={checkWordLen(this)}
 
+    
+
+    return (
+        <div>
+            <textarea placeholder='Start your post here...' className="w-full text-black-500 border rounded"></textarea>
+        </div>
     )
 }
 
 const Submit = () => {
     return (
-        <button>Submit</button>
+        <button className="float-right text-black-500 border border-black-500 hover:bg-blue-500 hover:text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Submit</button>
     );
 }
 
-export const PostDraft = () => {
+export const ExternalFiles = () => {
+    const [image, selectImage] = useState(false)
+    const [thumbsDown, setThumbsDown] = useState(false)
+    const [comment, setComment] = useState(false)
+
+    return (
+        <div className=" flex">
+            <button className="m-3 mt-7 ml-0" onClick={() => selectImage(!image)}>{image ? <BsImageFill size="20" /> : <BsImage size="20" />}</button>
+            <button className="m-3 mt-7 ml-0" onClick={() => setThumbsDown(!thumbsDown)}>{thumbsDown ? <PiThumbsDownFill size="20" /> : <PiThumbsDown size="20" />}</button>
+            <button className="m-3 mt-7 ml-0" onClick={() => setComment(!comment)}>{comment ? <BiSolidCommentDots size="20" /> : <BiCommentDetail size="20" />}</button>
+        </div>
+
+    )
+}
+
+const PostDraft = () => {
     return (
         <div className="m-auto p-10 shadow-md w-1/2 h-1/4">
             <div className="flex ">
@@ -28,8 +60,9 @@ export const PostDraft = () => {
                 <NameUsername />
             </div>
             <TextArea />
+            <ExternalFiles />
             <div>
-            <Submit />
+                <Submit />
             </div>
         </div>
 
