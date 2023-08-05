58a2f6a233c43a1ca16688c384244901717cf8cd
tonylxm <126369686+tonylxm@users.noreply.github.com>
2023-08-05 14:04:19 +1200

    Updated PostDraft with styling

--- medieval-twitter/src/components/PostDraft.js
+++ medieval-twitter/src/components/PostDraft.js
@@ -1,23 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { db } from '../firebase'; // Import your Firebase configuration file
 
-function PostDraft() {
+const Picture = () => {
+    return (
+        <>
+            <div className="overflow-hidden rounded-lg w-20">
+                <img src={require("../images/profile-picture.jpg")} alt={"Your profile pic"} className="rounded-full w-full h-20  object-cover" />
+            </div>
+        </>
+    );
+}
+
+const NameUsername = () => {
+    return (
+        <>
+            <h1>Lord Faarquard</h1>
+            <h3>@lordoftheland</h3>
+        </>
+    );
+}
+
+const Text = () => {
+    return (
+        <textarea placeholder='Start your post here...'></textarea>
+    )
+}
+
+const PostDraft = () => {
 
     return (
-    <div>
-        <div>
-            <h1>Profile Photo</h1>
+        <div className="m-auto p-20 shadow-md w-3/4 h-1/4">
+            <Picture />
+            <NameUsername />
+            <Text />
         </div>
-        <div>
-            <textarea>
-
-            </textarea>
-        </div>
-        <div>
-            <button>Submit</button>
-        </div>
-    </div>
-    );   
+    );
 }
 
 export default PostDraft;
