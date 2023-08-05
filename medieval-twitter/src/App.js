09d1bf783af06cabac9d19ac9f5e6c994fcdbbbc
dis-may <maygan275@gmail.com>
2023-08-05 20:50:32 +1200

    added parchment image

--- medieval-twitter/src/App.js
+++ medieval-twitter/src/App.js
@@ -5,7 +5,7 @@ import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Home from './components/Home';
 import FriendSystem from './components/Friends';
-import Leaderboard from './components/Leaderboard';
+// import Leaderboard from './components/Leaderboard'; 
 import { Post } from './components/Post';
 import Feed from './components/Feed';
 import Hierarchy from './components/Hierarchy';
@@ -20,7 +20,7 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/home" element={<Home />} />
         <Route path="/friends" element={<FriendSystem />} />
-        <Route path="/leaderboard" element={<Leaderboard />} />
+        {/* <Route path="/leaderboard" element={<Leaderboard />} />  */}
         
         <Route path="/post" element={<Post />} />
         <Route path="/feed" element={<Feed />} />
